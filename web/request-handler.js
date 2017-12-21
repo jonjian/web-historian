40515ea3d7bebfcd3d8b4795775106203a067b79
jonjian <jonjian@gmail.com>
2017-12-21 12:27:07 -0800

    redirect users to loading.html on POST

--- web/request-handler.js
+++ web/request-handler.js
@@ -14,49 +14,33 @@ exports.handleRequest = function (req, res) {
         res.write(data);
         res.end();
       });
-    } else {
-      // if site doesn't exist in file, add it to web/sites.txt
-        // and load "loading.html"
-      // if site exist in web/archives, load the .html
-      // post to file
-      serveAssets(res, archive.paths.archivedSites + myUrl.path, (data) => {
+    }
+  } else if (req.method === 'POST') {
+    var body = [];
+
+    req.on('data', (chunks) => {
+      body.push(chunks);
+    });
+
+    req.on('end', () => {
+      body = Buffer.concat(body).toString().slice(4);
+      serveAssets(res, archive.paths.archivedSites + '/' + body, (data) => {
         if (data !== undefined) {
-        //serve assets
+          //serve assets if data (html) is found
           res.writeHead(200, {'Content-Type': 'text/html'});
           res.write(data);
           res.end();
-          //render the html from archives/sites folder
-
         } else { 
-          // console.log(archive.paths.siteAssets);
+          // server loading.html asset if not found
           serveAssets(res, __dirname + '/public/loading.html', (data) => {
-            //render loading.html page
             //write url into sites.text
+            console.log(body);
             //cron will do the work in x minutes later
             res.writeHead(404, {'Content-Type': 'text/html'});
             res.write(data);
             res.end();
-            // add it to the urlList `exports.paths.list`
           });
         }
-      });
-     
-    }
-  }
-  if (req.method === 'POST') {
-    var body = [];
-    req.on('data', (chunks) => {
-      body.push(chunks);
-    });
-    req.on('end', () => {
-      body = Buffer.concat(body).toString().slice(4);
-      fs.open(archive.paths.list, 'a', (err, fd) => {
-        fs.write(fd, body + '\n', (err, written, string) => {
-          fs.close(fd, () => {
-            res.writeHead(302, {'Content-Type': 'text/html'});
-            res.end();
-          });
-        });
       });
     });
   }
